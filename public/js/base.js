c5006e8f262a66295ac0866299080b5ab187046a
Jeremie Paas <jeremie.paas@gmail.com>
2013-01-09 18:37:27 +0100

    layout

--- public/js/base.js
+++ public/js/base.js
@@ -181,17 +181,6 @@ $(function()
    
    
    dialogForgotPassword();
-   
-    // Google Analytics
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-37498192-1']);
-    _gaq.push(['_trackPageview']);
-
-    (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    })();
 });
 
 function checkSuccess(response)
