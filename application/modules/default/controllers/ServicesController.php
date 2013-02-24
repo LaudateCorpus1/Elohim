6e72d57d5243de01617f152a309ac168200db5e7
Jeremie <jeremie.paas@gmail.com>
2013-02-24 02:52:29 +0100

    Prayer times webservice

--- application/modules/default/controllers/ServicesController.php
+++ application/modules/default/controllers/ServicesController.php
@@ -14,7 +14,7 @@ class ServicesController extends Zend_Controller_Action
     public function prayertimesAction()
     {
         mb_internal_encoding('UTF-8');
-                
+               
         if(isset($_GET['city']))
                 $city = $_GET['city'];
         else
@@ -26,10 +26,23 @@ class ServicesController extends Zend_Controller_Action
                 $date = date('Y-m-d', time());
 
         if(isset($_GET['timezonename']))
-                $expires = gmdate('D, d M Y H:i:s \G\M\T', strtotime($_GET['timezonename']));
+        {
+            $timezoneName = str_replace('-', '/', $_GET['timezonename']);
+            $dateT = new DateTime(null, new DateTimeZone($timezoneName));
+            $year = $dateT->format('Y');
+            $month = $dateT->format('m');
+            $day = $dateT->format('d');
+            $offset = $dateT->format('P');
+            $offsets = explode(':', $offset);
+            $hourOffset = intval($offsets[0]);
+            $minutesOffset = intval($offsets[1]);
+//            $dateTimeZone = new DateTimeZone($_GET['timezonename']);
+//            $dateTime = new DateTime('now');
+            $expires = date('D, d M Y H:i:s \G\M\T', mktime(23 - $hourOffset, 59 - $minutesOffset, 59, intval($month), intval($day), intval($year)));
+        }
         else
-                $expires = gmdate('D, d M Y H:i:s \G\M\T', strtotime('Europe/Paris'));
-
+            $expires = gmdate('D, d M Y H:i:s \G\M\T', strtotime('Europe/Paris'));
+        
         if(isset($_GET['latitude']))
                 $latitude = $_GET['latitude'];
         else
@@ -60,7 +73,7 @@ class ServicesController extends Zend_Controller_Action
 
         $this->getResponse()->setHeader('X-WNS-Expires', $expires);
         
-        $xml = '<?xml version="1.0" encoding="utf-8"?><tile><visual><binding template="TileWideText02"><text id="1">'.$city.'</text><text id="2">Fajr '.$prayerTimes['Fajr'].'</text><text id="3">Asr '.$prayerTimes['Asr'].'</text><text id="4">Sunrise '.$prayerTimes['Sunrise'].'</text><text id="5">Maghrib '.$prayerTimes['Maghrib'].'</text><text id="6">Dhur '.$prayerTimes['Dhuhr'].'</text><text id="7">Isha '.$prayerTimes['Isha'].'</text></binding></visual></tile>';
+        $xml = '<?xml version="1.0" encoding="utf-8"?><tile><visual><binding template="TileWideText02"><text id="1">'.$city.'</text><text id="2">Fajr '.$prayerTimes['Fajr'].'</text><text id="3">Asr '.$prayerTimes['Asr'].'</text><text id="4">Shuruq '.$prayerTimes['Sunrise'].'</text><text id="5">Maghrib '.$prayerTimes['Maghrib'].'</text><text id="6">Dhur '.$prayerTimes['Dhuhr'].'</text><text id="7">Isha '.$prayerTimes['Isha'].'</text></binding></visual></tile>';
         echo $xml;
     }
 }
