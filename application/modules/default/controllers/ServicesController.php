ff867b2d1b5bea632b75feffc61fa968928d193d
Jeremie <jeremie.paas@gmail.com>
2013-03-12 13:28:38 +0100

    Prayer times webservice privacy policy

--- application/modules/default/controllers/ServicesController.php
+++ application/modules/default/controllers/ServicesController.php
@@ -113,5 +113,10 @@ class ServicesController extends Zend_Controller_Action
         $xml = '<?xml version="1.0" encoding="utf-8"?><tile><visual><binding template="TileWideText02"><text id="1">'.$city.'</text><text id="2">Fajr '.$prayerTimes['Fajr'].'</text><text id="3">Asr '.$prayerTimes['Asr'].'</text><text id="4">Shuruq '.$prayerTimes['Sunrise'].'</text><text id="5">Maghrib '.$prayerTimes['Maghrib'].'</text><text id="6">Dhur '.$prayerTimes['Dhuhr'].'</text><text id="7">Isha '.$prayerTimes['Isha'].'</text></binding></visual></tile>';
         echo $xml;
     }
+    
+    public function privacypolicyAction()
+    {
+        $this->_helper->layout->setLayout('index_layout');
+    }
 }
 
